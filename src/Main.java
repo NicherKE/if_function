b0dceab18bc94658e137f019130423222e8a769e
Nicherke <nicksoncheruiyot001@gmail.com>
2024-06-29 17:40:42 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 3.5;
+        double b = 3.5;
+        double c = 6;
+
+        if (b>a){
+            System.out.println("B is greater than A");
+        }
+        else if (b<a) {
+            System.out.println("B is smaller than A");
+        }
+
+         else{
+            System.out.println("B is equal to A");
+        }
+    }
+}
